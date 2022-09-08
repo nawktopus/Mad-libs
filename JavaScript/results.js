cc67484f20dfd27fb06c4a8c25b2ce735361c051
Sage Jasinski <sagejasinski@gmail.com>
2022-09-08 11:51:57 -0700

    conflicts resolved

--- JavaScript/results.js
+++ JavaScript/results.js
@@ -66,21 +66,15 @@ function origin (){
         pTwentyThree.textContent = values.inTwelve;
         pTwentyFour.textContent = values.inThirteen;
         //change <p> to the dog story and dog variables
-<<<<<<< HEAD
-
-        // console.log('dog is true')
-=======
-        
 
         console.log('dog is true');
->>>>>>> 501fb0a1cc7e11b74497506e38ee860c41858728
         dog = false;
         let stringDog = JSON.stringify(dog);
         localStorage.setItem('Dog', stringDog);
 
     }else if (cat === true){
         //if we came from cat
-<<<<<<< HEAD
+
         let catwords = localStorage.getItem('Catword');
         let value = JSON.parse(catwords);
 
@@ -105,8 +99,6 @@ function origin (){
         pNineteen.textContent = value.catTen;
         pTwenty.textContent = '. What a thoughtful kitten!';
 
-=======
->>>>>>> 501fb0a1cc7e11b74497506e38ee860c41858728
         //change <p> to cat story and cat variables
 
         console.log('cat is true');
@@ -120,7 +112,6 @@ function origin (){
         let halloweenWords = localStorage.getItem('HalloweenWords');
         let values = JSON.parse(halloweenWords);
 
-<<<<<<< HEAD
         pOne.textContent ='I can\'t believe it\s already';
         pTwo.textContent = values.inOne;
         pThree.textContent = '! I can\'t wait to put on my';
@@ -143,10 +134,6 @@ function origin (){
         pTwenty.textContent = '';
         pTwentyOne.textContent = 'are ready to go!';
         
-
-=======
-        
->>>>>>> 501fb0a1cc7e11b74497506e38ee860c41858728
         //change <p> Halloween story and halloween variables
 
         console.log('halloween is true');
